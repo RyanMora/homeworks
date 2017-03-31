2b8f09d1724df5fcab29324d3a3d8f5a7079a928
Ryan Mora <ryanmora@Ryans-MacBook-Pro.local>
2017-03-30 22:38:06 -0700

    completed Map class and homework

--- W1D4/DIY_ADTS.rb
+++ W1D4/DIY_ADTS.rb
@@ -43,23 +43,26 @@ class Map
   end
 
   def assign(key, value)
-
+    el = @map.index {|pair| pair[0] == key}
+    el ? @map[el][1] = value : @map.push([key, value])
+    [key, value]
   end
 
   def lookup(key)
-
+    @map.each {|pair| return pair[1] if pair[0] == key}
+    nil
   end
 
   def remove(key)
-
+    @map.reject! {|pair| pair[0] == key}
   end
 
   def show
-
+    deep_dup(@map)
   end
 
-  def assign
-
+  private
+  def deep_dup(arr)
+    arr.map {|el| el.is_a?(Array) ? deep_dup(el) : el}
   end
-
 end
