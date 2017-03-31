71c869d2b8ab44d46354ab3ed8e3e320e96c5614
Ryan Mora <ryanmora@Ryans-MacBook-Pro.local>
2017-03-30 22:19:02 -0700

    added DIY_ADTS.rb

---
+++ W1D4/DIY_ADTS.rb
@@ -0,0 +1,32 @@
+class Stack
+  def initialize
+    @stack = []
+  end
+
+  def add(el)
+    @stack << el
+    el
+  end
+
+  def remove
+    @stack.pop
+  end
+
+  def show
+    @stack.dup
+  end
+end
+
+class Queue
+  def enqueue(el)
+
+  end
+
+  def dequeue
+
+  end
+
+  def show
+
+  end
+end
