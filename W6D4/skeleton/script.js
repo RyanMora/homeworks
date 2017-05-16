4baf9023b8d3da4f39a7380425dd6d30653593f2
RyanMora <ryan.aimco@gmail.com>
2017-05-16 09:13:20 -0700

    finished phase 2

--- W6D4/skeleton/script.js
+++ W6D4/skeleton/script.js
@@ -19,7 +19,22 @@ document.addEventListener("DOMContentLoaded", () => {
   // adding SF places as list items
 
   // --- your code here!
+  const handleFavoriteSubmit = (e) => {
+    e.preventDefault();
 
+    const favoriteInput = document.querySelector(".favorite-input");
+    const favorite = favoriteInput.value;
+    favoriteInput.value = "";
+
+    const newListLi = document.createElement("li");
+    newListLi.textContent = favorite;
+
+    const favoritesList = document.getElementById("sf-places");
+    favoritesList.appendChild(newListLi);
+  };
+
+  const listSubmitButton = document.querySelector(".favorite-submit");
+  listSubmitButton.addEventListener("click", handleFavoriteSubmit);
 
 
   // adding new photos
