0ada94049b09c1819280dca380032b00dec28955
RyanMora <ryan.aimco@gmail.com>
2017-05-19 09:10:24 -0700

    finished phase 2 - refactoring

--- W7D2/solution_1/bundle.js
+++ W7D2/solution_1/bundle.js
@@ -12784,20 +12784,33 @@ document.addEventListener('DOMContentLoaded', function () {
   var preloadedState = localStorage.state ? JSON.parse(localStorage.state) : {};
   var store = (0, _store2.default)(preloadedState);
 
-  store.dispatch = addLoggingToDispatch(store);
+  store = applyMiddlewares(store, addLoggingToDispatch);
 
   var root = document.getElementById('content');
   _reactDom2.default.render(_react2.default.createElement(_root2.default, { store: store }), root);
 });
 
 var addLoggingToDispatch = function addLoggingToDispatch(store) {
-  var OGDispatch = store.dispatch;
-  return function (action) {
-    console.log(store.getState());
-    console.log(action);
-    OGDispatch(action);
-    console.log(store.getState());
+  return function (next) {
+    return function (action) {
+      console.log(store.getState());
+      console.log(action);
+      next(action);
+      console.log(store.getState());
+    };
   };
+};
+
+var applyMiddlewares = function applyMiddlewares(store) {
+  for (var _len = arguments.length, middlewares = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    middlewares[_key - 1] = arguments[_key];
+  }
+
+  var dispatch = store.dispatch;
+  middlewares.forEach(function (middleware) {
+    dispatch = middleware(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch: dispatch });
 };
 
 /***/ }),
