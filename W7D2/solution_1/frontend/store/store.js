e37df44c8b7a178e4cbc7b001fa3aefc5dc183b0
RyanMora <ryan.aimco@gmail.com>
2017-05-19 09:16:42 -0700

    finished hw

--- W7D2/solution_1/frontend/store/store.js
+++ W7D2/solution_1/frontend/store/store.js
@@ -1,12 +1,25 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(RootReducer, preloadedState);
+  const store = createStore(RootReducer, preloadedState, applyMiddleware(addLoggingToDispatch, sillyMiddleware));
+
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
   return store;
+}
+
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+}
+
+const sillyMiddleware = store => next => action => {
+  console.log("Silly");
+  next(action);
 }
 
 export default configureStore;
--- W7D2/solution_1/frontend/todo_redux.jsx
+++ W7D2/solution_1/frontend/todo_redux.jsx
@@ -9,23 +9,6 @@ document.addEventListener('DOMContentLoaded', () => {
     JSON.parse(localStorage.state) : {};
   let store = configureStore(preloadedState);
 
-  store = applyMiddlewares(store, addLoggingToDispatch);
-
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
-
-const addLoggingToDispatch = store => next => action => {
-  console.log(store.getState());
-  console.log(action);
-  next(action);
-  console.log(store.getState());
-};
-
-const applyMiddlewares = (store, ...middlewares) => {
-  let dispatch = store.dispatch;
-  middlewares.forEach((middleware) => {
-    dispatch = middleware(store)(dispatch);
-  });
-  return Object.assign({}, store, { dispatch });
-};
